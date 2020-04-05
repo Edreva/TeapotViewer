9edde32998af1f71feaa8b1e245a6969c3060edd
Ewan Drever-Smith <43785765+EDrever-Smith@users.noreply.github.com>
2020-04-05 11:44:30 +0100

    Update mainwindow.h

--- Model_Viewer_Application/mainwindow.h
+++ Model_Viewer_Application/mainwindow.h
@@ -16,6 +16,7 @@
 
 //VTK libaries
 #include <vtkActor.h>
+#include <vtkAppendPolyData.h>
 #include <vtkAxesActor.h>
 #include <vtkBoxRepresentation.h>
 #include <vtkBoxWidget2.h>
@@ -34,6 +35,7 @@
 #include <vtkOrientationMarkerWidget.h>
 #include <vtkPlane.h>
 #include <vtkPlaneWidget.h>
+#include <vtkPNGWriter.h>
 #include <vtkPoints.h>
 #include <vtkPolyData.h>
 #include <vtkPolyDataMapper.h>
@@ -41,13 +43,17 @@
 #include <vtkPyramid.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
+#include <vtkRendererCollection.h>
 #include <vtkSTLReader.h>
+#include <vtkSTLWriter.h>
 #include <vtkShrinkFilter.h>
 #include <vtkSmartPointer.h>
 #include <vtkTetra.h>
 #include <vtkTransform.h>
+#include <vtkTriangle.h>
+#include <vtkTriangleFilter.h>
 #include <vtkUnstructuredGrid.h>
-#include <vtkRendererCollection.h>
+#include <vtkWindowToImageFilter.h>
 
 //C++ libaries
 #include <map>
@@ -137,8 +143,12 @@ public slots:
     void openSTL(QString);
     //function to open the filter editor dialog box
     void loadShrinkFilterDialog();
+    // convert MOD to STL
+    void conversion(Model*);
     //function to select which filter editor dialog to open
     void loadFilterEditor();
+    //function to allow the user to save a screenshot
+    void handleScreenshot();
 
 private:
     Ui::MainWindow *ui;
@@ -155,21 +165,22 @@ private:
     vtkSmartPointer<vtkAxesActor> axes;
     vtkSmartPointer<vtkOrientationMarkerWidget> orientationMarker;
     vtkSmartPointer<vtkPlane> plane;
-    //vtkSmartPointer<vtkPlaneWidget> planeWidget;
     vtkSmartPointer<vtkImplicitPlaneWidget2> planeWidget;
     vtkSmartPointer<vtkBoxWidget2> boxWidget;
     vtkSmartPointer<vtkBoxWidgetCallback> boxWidgetCallback;
     vtkSmartPointer<vtkPlaneWidgetCallback> planeWidgetCallback;
     vtkSmartPointer<vtkImplicitPlaneRepresentation> rep;
     vtkSmartPointer<vtkShrinkFilter> shrinkFilter;
-
-    //unique_ptr<dialogEditShrinkFilter> shrinkFilterDialog;
+  	vtkSmartPointer<vtkCellArray> cell;
+	  vtkSmartPointer<vtkPoints> pointData;
+    
     dialogEditShrinkFilter *shrinkFilterDialog;
 
     vector<double> value; // store the RGB value of light
     double intensity; // store the intensity of light
     vector<vtkSmartPointer<vtkActor>> primitiveShapeActor; // store all the actor for primitive shape
     vector<vtkSmartPointer<vtkActor>>::const_iterator shapeItor; // iterator for primitive shape
+
     QButtonGroup* filterButton; //button group to link radio buttons for the filter
 };
 #endif // MAINWINDOW_H
